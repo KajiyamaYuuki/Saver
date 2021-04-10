4392ac2919be9d9ecabe91f838571cb473dc6dbf
yuuki kajiyama <luv4u.1331@gmail.com>
2021-04-10 17:06:50 +0900

    会話情報のコントローラ作成・設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'conversations/index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show]
   get 'home/index'
   root 'home#index'
+  resources :conversations do
+    resources :messages
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
