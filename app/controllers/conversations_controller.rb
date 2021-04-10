42ac2ff5b8419f463f6199bf953b3fd8b5212b52
yuuki kajiyama <luv4u.1331@gmail.com>
2021-04-10 17:10:50 +0900

    メッセージのコントローラ作成・設定

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,8 +1,10 @@
 class ConversationsController < ApplicationController
   before_action :authenticate_user!
+
   def index
     @conversations = Conversation.all
   end
+
   def create
     if Conversation.between(params[:sender_id], params[:recipient_id]).present?
       @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
@@ -11,8 +13,10 @@ class ConversationsController < ApplicationController
     end
     redirect_to conversation_messages_path(@conversation)
   end
+
   private
   def conversation_params
     params.permit(:sender_id, :recipient_id)
   end
+
 end
