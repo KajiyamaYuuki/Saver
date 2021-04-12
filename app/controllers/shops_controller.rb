6a23005caa0641b6fa250ae5d282c14d231b9c82
yuuki kajiyama <luv4u.1331@gmail.com>
2021-04-12 16:17:03 +0900

    shopsコントローラのプライベートアクション設定

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,39 @@
 class ShopsController < ApplicationController
+  before_action :set_shop, only: %i[ show edit update destroy ]
+
   def index
     @shops = Shop.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def shop_params
+    params.require(:shop).permit(:name, :description, :post_code, :address, :phone_number, :email, :url, :image)
+  end
+
+  def set_shop
+    @shop = Shop.find(params[:id])
   end
 end
