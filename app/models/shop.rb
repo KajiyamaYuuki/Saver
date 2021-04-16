e61fd2fe070779c3d9c21a55f33436aa1a84b6ed
yuuki kajiyama <luv4u.1331@gmail.com>
2021-04-17 00:24:09 +0900

    review周りのCSS修正

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,6 +4,7 @@ class Shop < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :reviews, dependent: :destroy
   belongs_to :user
+
   include JpPrefecture
   jp_prefecture :prefecture_code
 
@@ -14,4 +15,21 @@ class Shop < ApplicationRecord
   def prefecture_name=(prefecture_name)
     self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
   end
+
+  def average_score
+    unless self.reviews.empty?
+      reviews.average(:score).round(1).to_f
+    else
+      0.0
+    end
+  end
+
+  def review_score_percentage
+    unless self.reviews.empty?
+      reviews.average(:score).round(1).to_f*100/5
+    else
+      0.0
+    end
+  end
+
 end
