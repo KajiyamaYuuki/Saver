de84adc8ed34cc5b4e2b9d800233f98c42e71b51
yuuki kajiyama <luv4u.1331@gmail.com>
2021-04-21 22:41:50 +0900

    userとshopのメッセージタブ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if @user.is_owner?
+      @rooms = Conversation.where(recipient_id: @user.id)
+    else
+      @rooms = Conversation.where(sender_id: @user.id)
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   has_one :shop, dependent: :destroy
   has_many :reservations, dependent: :destroy
   has_many :reviews, dependent: :destroy
+  has_many :conversations
+  has_many :messages
+
+  def rooms(user_id)
+    Conversation.where(sender_id: user_id)
+  end
 end
