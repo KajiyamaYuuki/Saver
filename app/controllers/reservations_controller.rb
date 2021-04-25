f5ce9b87e6d7f1a213d017c9b8fe9c67bfb90374
yuuki kajiyama <luv4u.1331@gmail.com>
2021-04-26 01:56:22 +0900

    reservations_spec作成・検証

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -14,7 +14,7 @@ class ReservationsController < ApplicationController
     @reservation = current_user.reservations.build(reservation_params)
     @reservation.menu_id = @menu.id
     if @reservation.save
-      redirect_to shop_path(@menu.shop_id)
+      redirect_to user_path(current_user.id)
     else
       render :new
     end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :menu
+  validates :start_scheduled_at, presence: true
+  validates :end_scheduled_at, presence: true
 end
