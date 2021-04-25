1287ff101fc728077d214fc61db2c1ca1972319d
yuuki kajiyama <luv4u.1331@gmail.com>
2021-04-25 22:59:14 +0900

    Menuの編集テストが通る

--- spec/system/menus_spec.rb
+++ spec/system/menus_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Menu, type: :system do
   let(:owner) { create :owner }
+  let(:shop) { create :shop, user: owner }
   describe 'Menu CRUD' do
-    before do
-      FactoryBot.create(:shop, user: owner)
-      login(owner)
-    end
-    describe 'Menuを新しく登録する' do
+    describe 'Menuが未登録' do
+      before do
+        FactoryBot.create(:shop, user: owner)
+        login(owner)
+      end
       describe 'Menu新規登録' do
         context 'フォームの入力値が正常' do
           it 'Menuの新規登録が成功' do
@@ -32,6 +33,40 @@ RSpec.describe Menu, type: :system do
             expect(page).to have_content 'メニュー名を入力してください'
             expect(page).to have_content '価格を入力してください'
             expect(page).to have_content '予約可否を入力してください'
+          end
+        end
+      end
+    end
+    describe 'Menuが登録済み' do
+      before do
+        FactoryBot.create(:menu, shop: shop)
+        login(owner)
+      end
+      describe 'Menu編集' do
+        context 'フォームの入力値が正常' do
+          it 'Menuの内容編集が成功' do
+            visit shop_path(owner.shop.id)
+            click_link 'shop-show-edit-menu'
+            fill_in 'shop-menu-title', with: 'オイル交換'
+            fill_in 'shop-menu-price', with: '3,000'
+            fill_in 'shop-menu-interval_hour', with: '0.5'
+            check 'shop-menu-reservationable'
+            click_button 'shop-menu-submit'
+            expect(current_path).to eq shop_path(owner.shop.id)
+            expect(page).to have_content 'オイル交換'
+          end
+        end
+        context 'フォームの入力値が不正' do
+          it 'Menuの内容編集が失敗' do
+            visit shop_path(owner.shop.id)
+            click_link 'shop-show-edit-menu'
+            fill_in 'shop-menu-title', with: nil
+            fill_in 'shop-menu-price', with: nil
+            fill_in 'shop-menu-interval_hour', with: '0.5'
+            check 'shop-menu-reservationable'
+            click_button 'shop-menu-submit'
+            expect(page).to have_content 'メニュー名を入力してください'
+            expect(page).to have_content '価格を入力してください'
           end
         end
       end
