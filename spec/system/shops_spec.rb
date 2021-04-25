66b5f20ae7351167731c7a5af40d100702bf1480
yuuki kajiyama <luv4u.1331@gmail.com>
2021-04-25 16:51:45 +0900

    menus_spec作成

--- spec/system/shops_spec.rb
+++ spec/system/shops_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Shop, type: :system do
               find('.shop-new-address_street').set('円山町28番4号')
               find('.shop-new-address_building').set('大場ビルA館')
               find('.shop-new-phone_number').set('03-5459-1808')
-              find('.shop-new-email').set('kajiyama@email.com')
+              find('.shop-new-email').set('testshop@email.com')
               find('.shop-new-url').set('https://diveintocode.jp')
               click_button 'shop-new-submit'
               expect(current_path).to eq shop_path(owner.shop.id)
@@ -40,11 +40,11 @@ RSpec.describe Shop, type: :system do
         end
     end
     describe 'Shopが既に登録済み' do
-      before do
-        FactoryBot.create(:shop, user: owner)
-        login(owner)
-      end
-        describe 'ショップ情報編集' do
+      describe 'ショップ情報編集' do
+        before do
+          FactoryBot.create(:shop, user: owner)
+          login(owner)
+        end
           context 'フォームの入力値が正常' do
             it 'Shopの情報編集が成功' do
               visit edit_shop_path(owner.shop.id)
@@ -60,7 +60,22 @@ RSpec.describe Shop, type: :system do
               expect(page).to have_content 'testshop'
             end
           end
-        end
+          context 'フォームの入力値が不正' do
+            it 'Shopの情報編集が失敗' do
+              visit edit_shop_path(owner.shop.id)
+              find('.shop-new-name').set('')
+              find('.shop-new-post_code').set('')
+              find('.shop-new-address_street').set('')
+              find('.shop-new-address_building').set('')
+              find('.shop-new-phone_number').set('')
+              find('.shop-new-email').set('')
+              find('.shop-new-url').set('')
+              click_button 'shop-new-submit'
+              expect(current_path).to eq shop_path(owner.shop.id)
+              expect(page).to have_content '名前を入力してください'
+            end
+          end
+      end
     end
   end
 end
